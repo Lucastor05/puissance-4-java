9a0ed3eba1922ffaaf757c9ae06de0fc51f82b42
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2023-01-16 12:05:35 +0100

    grid class

--- src/main/java/com/puissance4/modules/Grid.java
+++ src/main/java/com/puissance4/modules/Grid.java
@@ -10,12 +10,18 @@ public class Grid {
     static String[][] Table = new String[6][7];
     static boolean Play = true;
     static int Round = 1;
-    static String Sign = "X";
+    static Player player1;
+    static Player player2;
+    static Player actualPlayer;
+
 
     public void play() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nombre de joueurs ? (1 ou 2) :");
         int numberOfPlayers = sc.nextInt();
+        player1 = new Player("test", "X", "ff");
+        actualPlayer = player1;
+        player2 = new Player("test2", "O", "ff");
         Players = numberOfPlayers;
         while (Play) {
             if (numberOfPlayers == 2 || Round%2 != 0) {
@@ -33,7 +39,7 @@ public class Grid {
         String render = "\n";
         for (int i = 0; i <= Table.length-1; i++) {
             for (int j = 0; j <= Table[0].length-1; j++) {
-                if (Objects.equals(Table[i][j], "X") || Objects.equals(Table[i][j], "O")) {
+                if (Objects.equals(Table[i][j], player1.caractere) || Objects.equals(Table[i][j], player2.caractere)) {
                     render += Table[i][j];
                 } else {
                     render += "-";
@@ -59,20 +65,22 @@ public class Grid {
         int tempi = i;
         int tempj = j;
 
+        String sign = actualPlayer.caractere;
+
         //check left / right sides
-        while (tempj > 0 && Table[i][tempj-1] == Sign) {
+        while (tempj > 0 && Table[i][tempj-1] == sign) {
             rowCount += 1;
             tempj -= 1;
         }
         tempj = j;
-        while (tempj < 6 && Table[i][tempj+1] == Sign) {
+        while (tempj < 6 && Table[i][tempj+1] == sign) {
             rowCount += 1;
             tempj += 1;
         }
 
 
         //check down side
-        while (tempi < 5 && Table[tempi+1][j] == Sign) {
+        while (tempi < 5 && Table[tempi+1][j] == sign) {
             columnCount += 1;
             tempi += 1;
         }
@@ -80,14 +88,14 @@ public class Grid {
         //check diagonal right
         tempi = i;
         tempj = j;
-        while (tempi < 5 && tempj > 0 && Table[tempi+1][tempj-1] == Sign) {
+        while (tempi < 5 && tempj > 0 && Table[tempi+1][tempj-1] == sign) {
             diagonalRight += 1;
             tempi += 1;
             tempj -= 1;
         }
         tempi = i;
         tempj = j;
-        while (tempi > 0 && tempj < 6 && Table[tempi-1][tempj+1] == Sign) {
+        while (tempi > 0 && tempj < 6 && Table[tempi-1][tempj+1] == sign) {
             diagonalRight+= 1;
             tempi -= 1;
             tempj += 1;
@@ -96,14 +104,14 @@ public class Grid {
         //check diagonal left
         tempi = i;
         tempj = j;
-        while (tempi > 0 && tempj < 0 && Table[tempi-1][tempj-1] == Sign) {
+        while (tempi > 0 && tempj < 0 && Table[tempi-1][tempj-1] == sign) {
             diagonalLeft += 1;
             tempi -= 1;
             tempj -= 1;
         }
         tempi = i;
         tempj = j;
-        while (tempi < 5 && tempj < 6 && Table[tempi+1][tempj+1] == Sign) {
+        while (tempi < 5 && tempj < 6 && Table[tempi+1][tempj+1] == sign) {
             diagonalLeft += 1;
             tempi += 1;
             tempj += 1;
@@ -111,27 +119,28 @@ public class Grid {
 
         if (rowCount >= 4 || columnCount >= 4 || diagonalRight >= 4 || diagonalLeft >= 4) {
             sout();
-            System.out.println("Victoire des "+Sign);
+            System.out.println("Victoire de "+actualPlayer.pseudo);
             Play = false;
         }
 
-        if (Sign == "X") {
-            Sign = "O";
+        if (actualPlayer.equals(player1)) {
+            actualPlayer = player2;
         } else {
-            Sign = "X";
+            actualPlayer = player1;
         }
     }
 
     public void handleFall (int i) {
+        String sign = actualPlayer.caractere;
         boolean fallen = false;
         int length = 0;
         while (!fallen) {
-            if (Table[0][i] == "X" || Table[0][i] == "O") {
+            if (Table[0][i] == player1.caractere || Table[0][i] == player2.caractere) {
                 System.out.println("Cette colonne est pleine");
                 fallen = true;
             } else {
-                if (length == Table.length-1 || Table[length+1][i] == "X" || Table[length+1][i] == "O") {
-                    Table[length][i] = Sign;
+                if (length == Table.length-1 || Table[length+1][i] == player1.caractere || Table[length+1][i] == player2.caractere) {
+                    Table[length][i] = sign;
                     Round += 1;
                     fallen = true;
                 } else {
@@ -144,7 +153,7 @@ public class Grid {
 
     public void randomPlace () {
         int randomX = getRandomNumberUsingNextInt(0, Table[0].length);
-        while (Table[0][randomX] == "X" || Table[0][randomX] == "O") {
+        while (Table[0][randomX] == player1.caractere || Table[0][randomX] == player2.caractere) {
             randomX = getRandomNumberUsingNextInt(0, Table[0].length);
         }
         handleFall(randomX);
