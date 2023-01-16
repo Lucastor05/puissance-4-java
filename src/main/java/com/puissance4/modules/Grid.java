907f843797f7dd5f56f6e81008a91f171ab63975
nom1 <nom1@gmail.com>
2023-01-16 14:27:11 +0100

    v2

--- src/main/java/com/puissance4/modules/Grid.java
+++ src/main/java/com/puissance4/modules/Grid.java
@@ -18,10 +18,35 @@ public class Grid {
     public void play() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nombre de joueurs ? (1 ou 2) :");
-        int numberOfPlayers = sc.nextInt();
-        player1 = new Player("test", "X", "ff");
+        Integer numberOfPlayers = sc.nextInt();
+        Scanner p1 = new Scanner(System.in);
+        System.out.println("Pseudo du joueur1 :");
+        String pseudoOfPlayers1 = p1.nextLine();
+        Scanner p2 = new Scanner(System.in);
+        System.out.println("Pseudo du joueur2 :");
+        String pseudoOfPlayers2 = p2.nextLine();
+        Scanner c1 = new Scanner(System.in);
+        System.out.println("Caractere du joueur1 :");
+        String caractereOfPlayers1 = c1.nextLine();
+        Scanner c2 = new Scanner(System.in);
+        System.out.println("Caractere du joueur2 :");
+        String caractereOfPlayers2 = c2.nextLine();
+        Scanner C1 = new Scanner(System.in);
+        System.out.println("Couleur du joueur1 :");
+        String couleurOfPlayers1 = C1.nextLine();
+        Scanner C2 = new Scanner(System.in);
+        System.out.println("Couleur du joueur2 :");
+        String couleurOfPlayers2 = C2.nextLine();
+
+
+
+        String cara1 = "X";
+        String cara2 = "O";
+
+
+        player1 = new Player(pseudoOfPlayers1, caractereOfPlayers1, couleurOfPlayers1);
         actualPlayer = player1;
-        player2 = new Player("test2", "O", "ff");
+        player2 = new Player(pseudoOfPlayers2, caractereOfPlayers2, couleurOfPlayers2);
         Players = numberOfPlayers;
         while (Play) {
             if (numberOfPlayers == 2 || Round%2 != 0) {
