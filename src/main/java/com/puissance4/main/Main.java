4ced4694f4e702768d6b205ee34e25a62fe702c3
Lucastor05 <lucasdeniel05@gmail.com>
2023-01-19 10:58:26 +0100

    condition menu avec check des valeurs en entré done

--- src/main/java/com/puissance4/main/Main.java
+++ src/main/java/com/puissance4/main/Main.java
@@ -11,79 +11,135 @@ import com.puissance4.modules.Top10;
 public class Main {
     public static void main(String[] args) {
         Grid grid = new Grid();
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_RED = "\u001B[31m";
 
-        Scanner menu = new Scanner(System.in);
-        System.out.println("Menu:");
-        System.out.println("Pour jouer tapé 1");
-        System.out.println("Pour voir le Top 10 des meilleurs parties de Puissance 4 tapé 2");
-        System.out.println("Pour quitter le jeu tapé 3");
-        System.out.println("Que souhaitez vous faire?");
-        Integer choixmenu = menu.nextInt();
-        if(choixmenu.equals(1)) {
-            Scanner menuparti = new Scanner(System.in);
-            System.out.println("Menu de parti:");
-            System.out.println("Pour jouer contre une IA tapé 1");
-            System.out.println("Pour jouer contre un autre joueur tapé 2");
+        boolean restart = true;
+        while(restart) {
+
+            boolean choixvalide = false;
+            System.out.println("Menu:");
+            System.out.println("Pour jouer tapé 1");
+            System.out.println("Pour voir le Top 10 des meilleurs parties de Puissance 4 tapé 2");
             System.out.println("Pour quitter le jeu tapé 3");
             System.out.println("Que souhaitez vous faire?");
-            Integer choixmenuparti= menuparti.nextInt();
-            if(choixmenuparti==1||choixmenuparti==2) {
-                Integer numberOfPlayers=choixmenuparti;
-                Scanner sc = new Scanner(System.in);
-                Player player1 = new Player("Player_1", "X", "");
-                player1.Playerspersonnalisation();
-                grid.setPlayer1(player1);
-                grid.setActualPlayer(player1);
 
 
-                if(numberOfPlayers == 2){
-                    Player player2 = new Player("joueur2", "O", "");
-                    player2.Playerspersonnalisation();
-                    grid.setPlayer2(player2);
-                }else{
-                    Ia bot = new Ia("BOT", "0", "", 4);
-                    bot.iaspersonnalisation();
-                    bot.getDifficulty();
-                    grid.setPlayer2(bot);
-                }
-                grid.setPlayers(numberOfPlayers);
-                while (grid.isPlay()) {
-                    if (numberOfPlayers == 2 || grid.getRound()%2 != 0) {
-                        grid.printGrid();
-                        System.out.println("Choisissez la colonne sur laquelle vous voulez jouer :");
-                        int i = sc.nextInt();
-                        grid.handleFall(i-1);
-                    } else {
-                        Ia player2 = (Ia)grid.getPlayer2();
-                        if(player2.getDifficulty()==1){
-                            grid.randomPlace();
-                        }else if(player2.getDifficulty()==2){
-                            grid.iaLvl2();
-                        }else if(player2.getDifficulty()==3){
-                            grid.iaLvl3();
-                        }else if(player2.getDifficulty()==4){
-                            grid.iaLvl4();
+            while (!choixvalide){
+                Scanner menu = new Scanner(System.in);
+                if (menu.hasNextInt()) {
+                    int choixmenu = menu.nextInt();
+
+                    if (choixmenu == 1) {
+                        boolean choixNbJoueurValide = false;
+
+                        System.out.println("Menu de parti:");
+                        System.out.println("Pour jouer contre une IA tapé 1");
+                        System.out.println("Pour jouer contre un autre joueur tapé 2");
+                        System.out.println("Pour quitter le jeu tapé 3");
+                        System.out.println("Que souhaitez vous faire?");
+
+                        while(!choixNbJoueurValide) {
+                            Scanner menuparti = new Scanner(System.in);
+                            if (menuparti.hasNextInt()) {
+                                int choixmenuparti = menuparti.nextInt();
+                                if (choixmenuparti == 1 || choixmenuparti == 2) {
+                                    Integer numberOfPlayers = choixmenuparti;
+                                    Scanner sc = new Scanner(System.in);
+                                    Player player1 = new Player("Player_1", "X", "");
+                                    player1.Playerspersonnalisation();
+                                    grid.setPlayer1(player1);
+                                    grid.setActualPlayer(player1);
+
+
+                                    if (numberOfPlayers == 2) {
+                                        Player player2 = new Player("joueur2", "O", "");
+                                        player2.Playerspersonnalisation();
+                                        grid.setPlayer2(player2);
+                                    } else {
+                                        Ia bot = new Ia("BOT", "0", "", 4);
+                                        bot.iaspersonnalisation();
+                                        bot.getDifficulty();
+                                        grid.setPlayer2(bot);
+                                    }
+
+
+                                    //lancement jeu avec parametre precedent
+                                    grid.setPlayers(numberOfPlayers);
+                                    while (grid.isPlay()) {
+                                        if (numberOfPlayers == 2 || grid.getRound() % 2 != 0) {
+                                            grid.printGrid();
+                                            System.out.println("Choisissez la colonne sur laquelle vous voulez jouer :");
+                                            int i = sc.nextInt();
+                                            grid.handleFall(i - 1);
+                                        } else {
+                                            Ia player2 = (Ia) grid.getPlayer2();
+                                            if (player2.getDifficulty() == 1) {
+                                                grid.randomPlace();
+                                            } else if (player2.getDifficulty() == 2) {
+                                                grid.iaLvl2();
+                                            } else if (player2.getDifficulty() == 3) {
+                                                grid.iaLvl3();
+                                            } else if (player2.getDifficulty() == 4) {
+                                                grid.iaLvl4();
+                                            }
+                                        }
+                                    }
+                                    choixNbJoueurValide = true;
+                                    choixvalide = true;
+                                } else if (choixmenuparti == 2) {
+                                    System.exit(0);
+                                }else{
+                                    System.out.println(ANSI_RED+"\nNombre Invalide! Veuillez réessayer!"+ANSI_RESET);
+                                }
+                            }else{
+                                System.out.println(ANSI_RED+"\nCeci n'est pas un nombre! Veuillez réessayer!"+ANSI_RESET);
+                            }
                         }
+
+
+
+                    } else if (choixmenu == 2) {
+                        File logDir = new File("puissance-4-java");
+                        File[] files = logDir.listFiles();
+                        for (File file : files) {
+                            if (file.isFile() && file.getName().equals("File.txt")) {
+                                //1Top10.affichageTopTen();
+                                // Le fichier de log a été trouvé, vous pouvez faire quelque chose avec
+                                break;
+                            } else {
+                                System.out.println("il ny a pas encore de tableau des scores");
+                            }
+                        }
+                        choixvalide = true;
+                    } else if (choixmenu == 3) {
+                        System.exit(0);
+                    }else{
+                        System.out.println(ANSI_RED+"\nNombre Invalide! Veuillez réessayer!"+ANSI_RESET);
                     }
-                }
-            } else if (choixmenuparti==2) {
-                System.exit(0);
-            }
-        } else if (choixmenu.equals(2)) {
-            File logDir = new File("puissance-4-java");
-            File[] files = logDir.listFiles();
-            for (File file : files) {
-                if (file.isFile() && file.getName().equals("File.txt")) {
-                    //1Top10.affichageTopTen();
-                    // Le fichier de log a été trouvé, vous pouvez faire quelque chose avec
-                    break;
-                }
-                else {
-                    System.out.println("il ny a pas encore de tableau des scores");
+                }else{
+                    System.out.println(ANSI_RED+"\nCeci n'est pas un nombre! Veuillez réessayer!"+ANSI_RESET);
                 }
             }
-        } else if (choixmenu.equals(3)) {
-            System.exit(0);
+
+
+
+
+            //demander de restart le jeu
+            System.out.println("Voulez-vous relancer la partie ? \n1) Oui \t2) Non");
+            Scanner restartScan = new Scanner(System.in);
+            if(restartScan.hasNextInt()){
+                int restartAnswer = restartScan.nextInt();
+                if(restartAnswer == 1){
+                    System.out.println("\n");
+                    restart = true;
+                }else{
+                    restart = false;
+                }
+            }else{
+                restart = false;
+            }
         }
+
     }
 }
--- src/main/java/com/puissance4/modules/Player.java
+++ src/main/java/com/puissance4/modules/Player.java
@@ -15,76 +15,112 @@ public class Player {
     }
     public void Playerspersonnalisation(){
         String caractereOfPlayers = "X";
-        String couleurOfPlayers="";
-
-        Scanner choixpseudo = new Scanner(System.in);
-        System.out.println("Voulez vous personnaliser les pseudo? \n1-Oui\n2-Non");
-        Integer choixpseudoplayers = choixpseudo.nextInt();
-
-        if(choixpseudoplayers==1) {
-            Scanner p1 = new Scanner(System.in);
-            System.out.println("Pseudo du joueur "+pseudo+" :");
-            this.pseudo = p1.nextLine();
-        }
-
-
-        Scanner scan = new Scanner(System.in);
-        System.out.println("\n\nVoulez vous personnaliser vos symboles ? \n1-Oui\n2-Non");
-        int choixsymboleplayer = scan.nextInt();
-
-
-        if(choixsymboleplayer == 1){
-            boolean caraBon = false;
-
-            while(!caraBon){
-                Scanner c1 = new Scanner(System.in);
-                System.out.println("Joueur  : Quel symbole voulez-vous ?");
-                this.caractere = c1.nextLine();
-
-                if(caractereOfPlayers.equals(".") || caractereOfPlayers.length() != 1){
-                    System.out.println("\nSymbole non valide! Veuillez réessayer !");
-                    caraBon = false;
-                }else{
-                    caraBon = true;
-                }
-
-            }
-        }
-
+        String couleurOfPlayers;
+        boolean valide = false;
         String ANSI_RESET = "\u001B[0m";
-        Scanner scancouleur = new Scanner(System.in);
-        System.out.println("\n\nVoulez vous personnaliser la couleur de vos symboles ? \n1-Oui\n2-Non");
-        int choixcouleurplayer = scancouleur.nextInt();
-        if(choixcouleurplayer == 1) {
-            boolean caracouleur = false;
-            while(!caracouleur){
-                Scanner C1 = new Scanner(System.in);
-                System.out.println("Couleur du joueur :");
-                couleurOfPlayers = C1.nextLine();
+        String ANSI_RED = "\u001B[31m";
 
-                if(couleurOfPlayers.equals("Bleu")){
-                    this.couleur= "\u001B[34m";
+
+        System.out.println("Voulez vous personnaliser les pseudo? \n1-Oui\n2-Non");
+
+
+
+        while(!valide){
+            Scanner choixpseudo = new Scanner(System.in);
+
+            if(choixpseudo.hasNextInt()) {
+                int choixpseudoplayers = choixpseudo.nextInt();
+                if (choixpseudoplayers == 1) {
+                    Scanner p1 = new Scanner(System.in);
+                    System.out.println("Pseudo du joueur " + pseudo + " :");
+                    this.pseudo = p1.nextLine();
                 }
-                if(couleurOfPlayers.equals("Jaune")){
-                    this.couleur= "\u001B[33m";
-                }
-                if(couleurOfPlayers.equals("Violet")){
-                    this.couleur= "\u001B[35m";
-                }
-                if(couleurOfPlayers.equals("Rouge")){
-                    this.couleur= "\u001B[31m";
-                }
-                if (!(couleurOfPlayers.equals("Rouge")||couleurOfPlayers.equals("Violet")||couleurOfPlayers.equals("Jaune")||couleurOfPlayers.equals("Bleu"))) {
-                    System.out.println("\nCouleur non valide car non reconnues! Veuillez réessayer !");
-                    caracouleur = false;
-                } else {
-                    caracouleur = true;
-                }
+                valide = true;
+            }else{
+                System.out.println(ANSI_RED+"\nCeci n'est pas un nombre! Veuillez réessayer!"+ANSI_RESET);
             }
+        }
 
+
+        valide =  false;
+
+        System.out.println("\n\nVoulez vous personnaliser vos symboles ? \n1-Oui\n2-Non");
+
+        while(!valide){
+            Scanner scan = new Scanner(System.in);
+            if(scan.hasNextInt()){
+                int choixsymboleplayer = scan.nextInt();
+                if(choixsymboleplayer == 1){
+                    boolean caraBon = false;
+
+                    while(!caraBon){
+                        Scanner c1 = new Scanner(System.in);
+                        System.out.println("Joueur  : Quel symbole voulez-vous ?");
+                        this.caractere = c1.nextLine();
+
+                        if(caractereOfPlayers.equals(".") || caractereOfPlayers.length() != 1){
+                            System.out.println("\nSymbole non valide! Veuillez réessayer !");
+                            caraBon = false;
+                        }else{
+                            caraBon = true;
+                        }
+
+                    }
                 }
+                valide = true;
+            }else{
+                System.out.println(ANSI_RED+"\nCeci n'est pas un nombre! Veuillez réessayer!"+ANSI_RESET);
             }
 
         }
+
+
+        System.out.println("\n\nVoulez vous personnaliser la couleur de vos symboles ? \n1-Oui\n2-Non");
+
+
+
+        valide = false;
+
+        while(!valide){
+            Scanner scancouleur = new Scanner(System.in);
+            if(scancouleur.hasNextInt()) {
+                int choixcouleurplayer = scancouleur.nextInt();
+                if (choixcouleurplayer == 1) {
+                    boolean caracouleur = false;
+                    while (!caracouleur) {
+                        Scanner C1 = new Scanner(System.in);
+                        System.out.println("Couleur du joueur :\nVous pouvez choisir parmis les couleurs suivantes : [Bleu, Jaune, Violet et Rouge]");
+
+                        couleurOfPlayers = C1.nextLine();
+
+                        if (couleurOfPlayers.toLowerCase().equals("bleu")) {
+                            this.couleur = "\u001B[34m";
+                        }
+                        if (couleurOfPlayers.toLowerCase().equals("jaune")) {
+                            this.couleur = "\u001B[33m";
+                        }
+                        if (couleurOfPlayers.toLowerCase().equals("violet")) {
+                            this.couleur = "\u001B[35m";
+                        }
+                        if (couleurOfPlayers.toLowerCase().equals("rouge")) {
+                            this.couleur = "\u001B[31m";
+                        }
+                        if (!(couleurOfPlayers.toLowerCase().equals("rouge") || couleurOfPlayers.toLowerCase().equals("violet") || couleurOfPlayers.toLowerCase().equals("jaune") || couleurOfPlayers.toLowerCase().equals("bleu"))) {
+                            System.out.println(ANSI_RED+"\nCouleur non valide car non reconnues! Veuillez réessayer !"+ANSI_RESET);
+                            caracouleur = false;
+                        } else {
+                            caracouleur = true;
+                        }
+                    }
+
+                }
+                valide = true;
+            }else{
+                System.out.println(ANSI_RED+"\nCeci n'est pas un nombre! Veuillez réessayer!"+ANSI_RESET);
+            }
+        }
+    }
+
+}
 
 
